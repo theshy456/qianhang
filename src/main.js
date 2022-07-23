b9cb3634fe400245508289112108acc33bd666d8
theshy456 <lbs521lly@gmail.com>
2022-07-23 15:15:45 +0800

    手机版前航站点

---
+++ src/main.js
@@ -0,0 +1,61 @@
+// console.log($)
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+const hashMap = xObject || [
+    { logo: 'A', url: 'https://www.acfun.cn' },
+    { logo: 'B', url: 'https://www.bilibili.com' }
+]
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '') // 删除 / 开头的内容
+}
+const render = () => {
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li>
+            <div class="site">
+            <div class="logo">${node.logo}</div>
+                <div class="link">${simplifyUrl(node.url)}</div>
+                <div class="close">
+                    <svg class="iconpark-icon"><use href="#close-one"></use></svg>
+                </div>
+            </div>
+    `).insertBefore($lastLi)
+        $li.on('click', (e) => {
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation()//阻止冒泡
+            hashMap.splice(index, 1)
+            render()
+        })
+    })
+}
+render()
+
+
+$('.addButton')
+    .on('click', () => {
+        let url = window.prompt('请问你添加的网址是啥')
+        console.log(url)
+        if (url.indexOf('http') !== 0) {
+            // alert('请输入http开头的网址')
+            url = 'https://' + url
+        }
+        console.log(url)
+        hashMap.push({
+            logo: simplifyUrl(url)[0].toUpperCase(),
+            logoType: 'text',
+            url: url
+        });
+        render()
+    })
+window.onbeforeunload = () => {
+    // console.log('yemianguanbile')
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x', string)
+}
